a60ff248c6180d88df43b5be665cb3066816c42e
jinsong <jinsong@jiedaibao.com>
2020-05-26 18:50:13 +0800

    add ddl

--- src/main/java/com/lijiaqi/regdemo/controller/UserController.java
+++ src/main/java/com/lijiaqi/regdemo/controller/UserController.java
@@ -3,9 +3,13 @@ package com.lijiaqi.regdemo.controller;
 import com.lijiaqi.regdemo.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import com.lijiaqi.regdemo.services.UserService;
+
+import java.io.File;
 
 @RestController
 public class UserController {
@@ -15,14 +19,26 @@ public class UserController {
 
     @RequestMapping("adduser")
     @ResponseBody
-    public String addUser(String name,String mobile,String email,String password,String lisenceFilePath){
-            User user = new User();
-            user.setName(name);
-            user.setMobile(mobile);
-            user.setEmail(email);
-            user.setPassword(password);
-            user.setLisenceFilePath(lisenceFilePath);
-            int rows = userService.insert(user);
-            return "操作完成，受影响行数为"+rows+"行";
+    public String addUser(String name,String mobile,String email,String password,@RequestParam("file") MultipartFile file){
+
+        String fileName = file.getOriginalFilename();
+        String filePath = "/Users/jinsong/IdeaProjects/regdemo/src/main/resources/uploadFiles/";
+        String fullPath =  filePath+fileName;
+        File dest = new File(fullPath);
+        try {
+            file.transferTo(dest);
+        }catch(Exception e){
+            System.out.println(e.toString());
+            return "上传文件失败！";
+        }
+        User user = new User();
+        user.setName(name);
+        user.setMobile(mobile);
+        user.setEmail(email);
+        user.setPassword(password);
+        user.setLisenceFilePath(fullPath);
+        int rows = userService.insert(user);
+        return "操作完成，受影响行数为"+rows+"行";
+
     }
 }
