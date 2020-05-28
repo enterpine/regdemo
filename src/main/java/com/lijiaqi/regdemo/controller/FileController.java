a8bfa24802d4fd19dfbbeef2c4876ecafab80165
jinsong <jinsong@jiedaibao.com>
2020-05-28 11:04:17 +0800

    ad

--- src/main/java/com/lijiaqi/regdemo/controller/FileController.java
+++ src/main/java/com/lijiaqi/regdemo/controller/FileController.java
@@ -27,17 +27,16 @@ public class FileController {
     @Autowired
     private UserService userService;
 
-    @RequestMapping("/download/{id}")
+    @GetMapping("review/download/{id}")
     public String download(@PathVariable("id") int id,HttpServletResponse response){
         try {
             String filepath = userService.getUserById(id).getLisenceFilePath();
+            String[] str=filepath.split("/");
+            String filename = str[str.length-1];
             File file=new File(filepath);
-            // 创建输入流，传入文件对象
             FileInputStream fis =new FileInputStream(file);
-            // 设置相关格式
             response.setContentType("application/force-download");
-            // 设置下载后的文件名以及header
-            response.addHeader("Content-disposition", "attachment;filename=123.jpg");
+            response.addHeader("Content-disposition", "attachment;filename="+filename);
             OutputStream os = response.getOutputStream();
             // 常规操作
             byte[] buf = new byte[1024];
